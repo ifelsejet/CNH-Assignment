cd0985bd5141a997b4ea6dfa7a54beb76f81df74
Jatnael Montes <ifelsejet@gmail.com>
2020-08-29 14:16:54 -0700

    Added a second page, learned navigation

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+
+function HomeScreen ({ navigation }) {
+  return (
+    <View style={styles.container}>
+    <Text style={styles.h1}>Welcome to my landing page!</Text>
+    
+      <Button
+        title="LET'S START"
+        onPress={() => navigation.navigate('Details')}
+        style={styles.button}
+        color="gray"
+      />
+    <StatusBar style="auto" />
+  </View>
+  )
+}
+
+function DetailsScreen({navigation}) {
+  return (
+    <View style={styles.container}>
+    <Text style={styles.h1}>Welcome to my new page!</Text>
+    <Button
+        title="GO BACK HOME"
+        onPress={() => navigation.goBack()}
+        style={styles.button}
+        color="gray"
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.h1}>Welcome to my landing page!</Text>
-      
-        <Button
-          title="LET'S START"
-          onPress={() => this.onPress()}
-          style={styles.button}
-          color="gray"
-        />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
